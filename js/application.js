93e1844ba71033564eb13bf675f8691fcf32c558
Trystan Eveno <trystan.eveno@lephare.com>
2017-09-12 12:35:41 +0200

    start god anger

--- js/application.js
+++ js/application.js
@@ -1,4 +1,8 @@
 $( document ).ready(function() {
+    document.getElementById("open-eyes").addEventListener("click", eyes);
+    document.getElementById("song").addEventListener("mouseover", song);
+    document.getElementById("song").addEventListener("mouseleave", mute);
+
     setTimeout(function() {
         randomSounds();
         setInterval(function() {
@@ -8,57 +12,94 @@ $( document ).ready(function() {
 });
 
 function randomSounds() {
-    /***
-    *   rdm = 1, son blanc
-    *   rdm = 2, son bleu
-    *   rdm = 3, son noir
-    *   rdm = 4, son rouge
-    *   rdm = 5, son vert
-    ***/
+    if(sessionStorage.getItem('block-all') !== 'true') {
+        var rdm = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
 
-    var audio,
-        volume = 0,
-        rdm = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
-
-    if(rdm +"" === sessionStorage.getItem('rdm')) {
-        if(rdm === 5) {
-            rdm -= 1;
-        } else {
-            rdm += 1;
+        if(rdm +"" === sessionStorage.getItem('rdm')) {
+            if(rdm === 5) {
+                rdm -= 1;
+            } else {
+                rdm += 1;
+            }
         }
-    }
 
-    console.log(rdm);
-    sessionStorage.setItem('rdm', rdm);
-    if(rdm === 1) {
-        audio = new Audio('./son/blanc.mp3');
-        white();
-    } else if(rdm === 2) {
-        audio = new Audio('./son/bleu.mp3');
-        blue();
-    } else if(rdm === 3) {
-        audio = new Audio('./son/noir.mp3');
-        black();
-    } else if(rdm === 4) {
-        audio = new Audio('./son/rouge.mp3');
-        red();
-    } else if(rdm === 5) {
-        audio = new Audio('./son/vert.mp3');
-        green();
-    }
-    audio.play();
+        sessionStorage.setItem('rdm', rdm);
 
+        if(sessionStorage.getItem('song') !== 'true') {
+            if(rdm === 1) {
+                white();
+            } else if(rdm === 2) {
+                blue();
+            } else if(rdm === 3) {
+                black();
+            } else if(rdm === 4) {
+                red();
+            } else if(rdm === 5) {
+                green();
+            }
+        }
+
+        setTimeout(function() {
+            init_state();
+        }, 15000);
+    }
+}
+
+function eyes() {
+    var eyes = document.getElementById("eyes"),
+        img = document.getElementById("eyes-img"),
+        body = document.getElementsByTagName("body")[0];
+
+    sessionStorage.setItem('block-all', 'true');
+    init_state();
+
+    eyes.style.display = "block";
     setTimeout(function() {
-        audio.pause();
-        audio.currentTime = 0;
-        init_state();
-    }, 15000);
+        eyes.style.opacity = 1;
+    }, 100);
+}
+
+function showSpeaker() {
+    document.getElementById("speaker").style.display = "block";
+}
+
+function hideSpeaker() {
+    document.getElementById("speaker").removeAttribute("style");
+}
+
+function song() {
+    init_state();
+    showSpeaker();
+    sessionStorage.setItem('song', 'true');
+    document.getElementById("voyelles").play();
+}
+
+function mute() {
+    hideSpeaker();
+    sessionStorage.removeItem('song');
+    document.getElementById("voyelles").pause();
 }
 
 function init_state() {
     var spans = document.getElementsByTagName("span");
 
-    document.getElementsByTagName("body")[0].removeAttribute("style");
+    if(sessionStorage.getItem('song', 'true') !== 'true') {
+        hideSpeaker();
+    }
+    document.getElementById("overlay").style.opacity = "0";
+    document.getElementById("noir").pause();
+    document.getElementById("noir").currentTime = 0;
+    document.getElementById("bleu").pause();
+    document.getElementById("bleu").currentTime = 0;
+    document.getElementById("vert").pause();
+    document.getElementById("vert").currentTime = 0;
+    document.getElementById("rouge").pause();
+    document.getElementById("rouge").currentTime = 0;
+    document.getElementById("blanc").pause();
+    document.getElementById("blanc").currentTime = 0;
+    setTimeout(function() {
+        document.getElementById("overlay").removeAttribute("style");
+    }, 500);
 
     for (var i = 0; i < spans.length; i++) {
         spans[i].removeAttribute("style");
@@ -69,7 +110,10 @@ function black() {
     var time = 0.3,
         blacks = document.getElementsByClassName("black");
 
-    document.getElementsByTagName("body")[0].style.backgroundImage = "url('./img/nuit.gif')";
+    document.getElementById("noir").play();
+    showSpeaker();
+    document.getElementById("overlay").style.backgroundImage = "url('./img/nuit.gif')";
+    document.getElementById("overlay").style.opacity = "1";
 
     for (var i = 0; i < blacks.length; i++) {
         blacks[i].style.transitionDelay = time +'s';
@@ -81,7 +125,10 @@ function blue() {
     var time = 0.3,
         blue = document.getElementsByClassName("blue");
 
-    document.getElementsByTagName("body")[0].style.backgroundImage = "url('./img/jesus.gif')";
+    document.getElementById("bleu").play();
+    showSpeaker();
+    document.getElementById("overlay").style.backgroundImage = "url('./img/clairon.gif')";
+    document.getElementById("overlay").style.opacity = "1";
 
     for (var i = 0; i < blue.length; i++) {
         blue[i].style.transitionDelay = time +'s';
@@ -93,7 +140,10 @@ function green() {
     var time = 0.3,
         green = document.getElementsByClassName("green");
 
-    document.getElementsByTagName("body")[0].style.backgroundImage = "url('./img/riviere.gif')";
+    document.getElementById("vert").play();
+    showSpeaker();
+    document.getElementById("overlay").style.backgroundImage = "url('./img/riviere.gif')";
+    document.getElementById("overlay").style.opacity = "1";
 
     for (var i = 0; i < green.length; i++) {
         green[i].style.transitionDelay = time +'s';
@@ -105,7 +155,10 @@ function red() {
     var time = 0.3,
         red = document.getElementsByClassName("red");
 
-    document.getElementsByTagName("body")[0].style.backgroundImage = "url('./img/pluie.gif')";
+    document.getElementById("rouge").play();
+    showSpeaker();
+    document.getElementById("overlay").style.backgroundImage = "url('./img/pluie.gif')";
+    document.getElementById("overlay").style.opacity = "1";
 
     for (var i = 0; i < red.length; i++) {
         red[i].style.transitionDelay = time +'s';
@@ -117,7 +170,10 @@ function white() {
     var time = 0.3,
         white = document.getElementsByClassName("white");
 
-    document.getElementsByTagName("body")[0].style.backgroundImage = "url('./img/soleil.gif')";
+    document.getElementById("blanc").play();
+    showSpeaker();
+    document.getElementById("overlay").style.backgroundImage = "url('./img/soleil.gif')";
+    document.getElementById("overlay").style.opacity = "1";
 
     for (var i = 0; i < white.length; i++) {
         white[i].style.transitionDelay = time +'s';
